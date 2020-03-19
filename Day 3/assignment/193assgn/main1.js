d39f5122a0ffec8aa5c749193a06be4a3ff915a7
Vyshak 2020 <vyshaksa@gmail.com>
2020-03-19 22:26:30 +0530

    my first commit

---
+++ Day 3/assignment/193assgn/main1.js
@@ -0,0 +1,17 @@
+let n;
+function skeleton(n)
+{
+let fortune=["you are going to be rich","you will die at 70 years","you will get a good job","you will become a millionare"];
+console.log(fortune[n]);
+}
+let x,num;
+x=Math.random();
+console.log(x);
+x=x*10;
+x= parseInt(x);
+num=x%4;
+console.log(num);
+skeleton(num);
+
+
+
