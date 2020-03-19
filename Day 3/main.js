e42d7726f9ad5435455fbdd44b90b9ae135b7380
rejanirajesh <rejani2906@gmail.com>
2020-03-19 16:53:13 +0530

    Online fortune prediction

--- Day 3/main.js
+++ Day 3/main.js
@@ -1,10 +1,11 @@
+console.log("Refresh the page for new prediction --> ")
 function skelton(){
   var array=["You will become a millionare","You might get sick today","Today will be a good day",
   "Dont get out of home today","This year is going to affect you","Kindness will be repayed back to you today",
   "You will get a new pet","You will have fun today","You will meet someone unexpected today",
   "Something unexpected might happen","You will win the jackpot !!!"];
-  var num = Math.floor((Math.random() * 10)+ 1);
+  var num = Math.floor((Math.random() * 10)+ 1); // Generates a random number between 1 and 10
   let element=array[num];
-  console.log("Your fortune : "+(element));
+  console.log(" Your fortune : "+(element));
 }
 skelton();
