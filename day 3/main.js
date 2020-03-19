130abea9ead8902b936a6d502647785669f8ef5e
Niranjana M N <niranjanamuriamangalam@gmail.com>
2020-03-19 23:54:10 +0530

    added

---
+++ day 3/main.js
@@ -0,0 +1,12 @@
+let n;
+function skeleton(n)
+{
+var myArray=["love","friendship","travel","wealth","beauty"];
+console.log(myArray[n]);
+
+}
+let x=Math.random();
+x=x*10;
+x=parseInt(x);
+num=x%5;
+skeleton(num);
