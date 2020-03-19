73f61e1eb5c8c21227051e7d533778fc38755952
roniyavarghese <roniyavarghese2000@gmail.com>
2020-03-19 17:05:14 +0530

    uploading files

---
+++ Day 3/main.js
@@ -0,0 +1,10 @@
+function skelton(){
+  let array =["Change yourself and fortune will change.","Good things come when you least expect them.","Good luck beats early rising.","U may win the battle.","Fortune favors the prepared mind.","Your dream may come true."];
+  let l=array.length;
+  console.log("Length of the array="+l);
+    let x=Math.floor(Math.random() * 6);
+    console.log(x);
+    let a=array[x];
+    console.log(a);
+  }
+skelton();
