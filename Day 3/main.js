d2a91eff255c47eb159abc14819e2835b113f39c
unknown <ajaynair000@gmail.com>
2020-03-19 21:51:58 +0530

    ASSIGNMENT FINISHED-main.js

---
+++ Day 3/main.js
@@ -0,0 +1,6 @@
+function fortune(){
+    let array=["Life-ahead is timeless fortune.","Friends and acquaintances are the surest passport to fortune.","A secret admirer will soon send you a sign of affection.","Your heart is in a place to draw true happiness.","the children of misfortune come into their own","your lifeline is very short","Death is near you"];
+    var  x=Math.floor(Math.random() * 7);
+    console.log(array[x]);
+};
+fortune();
