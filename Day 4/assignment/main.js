3026d384f69fe6e22c3a3e5b77f9942dc4c4caa2
ananyams123 <ananyams001@gmail.com>
2020-03-20 22:02:54 +0530

    assignmentday4

---
+++ Day 4/assignment/main.js
@@ -0,0 +1,21 @@
+  
+  var fishstyle;   
+     
+ fishstyle = document.getElementById("fish");     
+ var i=fish.getBoundingClientRect().left;
+    var id = setInterval(swim, 5);
+ 
+   function swim() {      
+   if(i==800){
+	
+	i=0;
+	
+	
+	
+	}
+	else{
+ 	i++;
+	fishstyle.style.left = i + "px";   
+
+  }   
+}
